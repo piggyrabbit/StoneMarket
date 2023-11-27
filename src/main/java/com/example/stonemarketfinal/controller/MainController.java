78749d7bcc19aca01724db48c110209dfd828e2f
piggyrabbit <piggyrabbit@naver.com>
2023-11-27 15:11:16 +0900

    샘플 페이지 생성 및 샘플 통합 열람 페이지 생성

--- src/main/java/com/example/stonemarketfinal/controller/MainController.java
+++ src/main/java/com/example/stonemarketfinal/controller/MainController.java
@@ -6,16 +6,57 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-    @RequestMapping(value = "/test")
+    @RequestMapping(value = "/all") // 모든 예시 화면 링크 모음
+    public String all() {
+        return "all";
+    }
+
+    @RequestMapping(value = "/test") // 메인 화면
     public String test() {
         return "index";
     }
 
-    @RequestMapping(value = "shopList")
+    @RequestMapping(value = "shopList") // 상품 목록 검색 결과
     public String test2() {
         return "shopList";
     }
 
-    @RequestMapping(value = "test1")
-    public String test3() { return "shopList"; }
+    @RequestMapping(value = "about") // 사이트 설명
+    public String test3() { return "about"; }
+
+    @RequestMapping(value = "account-notification") // 계정 알림 설정
+    public String test4() { return "account-notification"; }
+
+    @RequestMapping(value = "account-orders") // 주문한 상품 목록
+    public String test5() { return "account-orders"; }
+
+    @RequestMapping(value = "account-settings") // 계정 설정
+    public String test6() { return "account-settings"; }
+
+    @RequestMapping(value = "contact") // 유저 문의 등록
+    public String test7() { return "contact"; }
+
+    @RequestMapping(value = "shop-filter") // 검색 기준 설정
+    public String test8() { return "shop-filter"; }
+
+    @RequestMapping(value = "shop-single-2") // 상품 상세정보
+    public String test9() { return "shop-single-2"; }
+
+    @RequestMapping(value = "shop-wishlist") // 위시리스트 목록
+    public String test10() { return "shop-wishlist"; }
+
+    @RequestMapping(value = "signin") // 로그인
+    public String test11() { return "signin"; }
+
+    @RequestMapping(value = "signup") // 회원가입
+    public String test12() { return "signup"; }
+
+    @RequestMapping(value = "store-grid") // 판매자 목록
+    public String test13() { return "store-grid"; }
+
+    @RequestMapping(value = "store-single") // 판매자 상세정보
+    public String test14() { return "store-single"; }
+
+    @RequestMapping(value = "add-product") // 상품 정보 추가
+    public String test15() { return "add-product"; }
 }
---
+++ src/main/webapp/WEB-INF/views/all.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<ul>
+    <li><a href="./test">메인 화면(작업중)</a></li>
+    - 검색창 초록색 효과 회색으로 변경<br>
+    - 오른쪽 상단의 메뉴 알림 목록, 위시리스트, 메시지 목록, 계정 설정으로 변경<br>
+    - 배너 크기 조정<br>
+    - 배너 넘기는 버튼 오른쪽, 왼쪽에 생성<br>
+    - 하단 내용 및 카테고리 내용 구상<br>
+    <li><a href="./shopList">상품 목록 검색 결과(작업중)</a></li>
+    <li><a href="./about">사이트 설명</a></li>
+    <li><a href="./account-notification">계정 알림 설정</a></li>
+    <li><a href="./account-orders">주문한 상품 목록</a></li>
+    <li><a href="./account-settings">계정 설정</a></li>
+    <li><a href="./contact">유저 문의 등록</a></li>
+    <li><a href="./shop-filter">검색 기준 설정</a></li>
+    <li><a href="./shop-single-2">상품 상세정보</a></li>
+    <li><a href="./shop-wishlist">위시리스트 목록</a></li>
+    <li><a href="./signin">로그인</a></li>
+    <li><a href="./signup">회원가입</a></li>
+    <li><a href="./store-grid">판매자 목록</a></li>
+    <li><a href="./store-single">판매자 상세정보</a></li>
+    <li><a href="./add-product">상품 정보 추가</a></li>
+
+</ul>
+
+어느 정도 작업이 완료된 후에는 대시보드 페이지 추가 작업(/dist/dashboard/)
+
+</body>
+</html>
