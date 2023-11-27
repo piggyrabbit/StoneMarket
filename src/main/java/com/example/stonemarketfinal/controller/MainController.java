8b001df5c9c43309db7084e23070d03de36252d1
piggyrabbit <piggyrabbit@naver.com>
2023-11-27 11:28:31 +0900

    버튼 색 및 테두리 디자인 수정

--- src/main/java/com/example/stonemarketfinal/controller/MainController.java
+++ src/main/java/com/example/stonemarketfinal/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-    @RequestMapping(value = "test")
+    @RequestMapping(value = "/test")
     public String test() {
         return "index";
     }
