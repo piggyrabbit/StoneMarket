904320a885296e55fc5231559b95a842b1213bfb
piggyrabbit <piggyrabbit@naver.com>
2023-11-27 09:50:16 +0900

    first upload

---
+++ src/main/java/com/example/stonemarketfinal/StoneMarketFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.stonemarketfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoneMarketFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoneMarketFinalApplication.class, args);
+    }
+
+}
